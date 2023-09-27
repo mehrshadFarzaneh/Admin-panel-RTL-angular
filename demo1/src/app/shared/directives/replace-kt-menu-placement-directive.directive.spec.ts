979672d13e78563c9bff31d12ef11247e6de2f30
Mehrshad Farzaneh <50589065+mehrshadFarzaneh@users.noreply.github.com>
2023-09-27 17:26:26 +0330

    [INIT]

---
+++ demo1/src/app/shared/directives/replace-kt-menu-placement-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ReplaceKtMenuPlacementDirectiveDirective } from './replace-kt-menu-placement-directive.directive';
+
+describe('ReplaceKtMenuPlacementDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ReplaceKtMenuPlacementDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ demo1/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReplaceKtMenuPlacementDirectiveDirective } from './directives/replace-kt-menu-placement-directive.directive';
+
+
+
+@NgModule({
+  declarations: [
+    ReplaceKtMenuPlacementDirectiveDirective
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
