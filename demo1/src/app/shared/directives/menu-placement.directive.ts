0d646c55a7077b7d34b640c24273fd9e7a62936d
Mehrshad Farzaneh <50589065+mehrshadFarzaneh@users.noreply.github.com>
2023-09-30 06:02:18 +0330

    [Refactor] menu-place-meat pipe cleaned

--- demo1/src/app/shared/directives/menu-placement.directive.ts
+++ demo1/src/app/shared/directives/menu-placement.directive.ts
@@ -14,24 +14,15 @@ export class MenuPlacementDirective implements AfterViewInit {
   private translateService: TranslationService, @Inject(DOCUMENT) private document: Document,private router: Router) { }
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.appFacade.setLayoutDirection(LangUtil.isRtlLang(this.translateService.getSelectedLanguage()));
-    // this.translateService.getSelectedLanguage();
   }
   ngAfterViewInit() {
 
     this.appFacade.isLayoutDirectionRtl$.subscribe((rtl: boolean) => {
-
-      // this.initialPlacementAttributeByLocalStorage(rtl);
-      // setTimeout(() => {
-      // Call the function to fetch and update elements
-      // this.updateElementsWithPlacement(rtl);
       this.itsRtl = rtl;
       this.updateMenuPlacementBasedOnRTL(rtl);
       this.replaceBodyAttributeFor(rtl);
       this.replaceMainStyleFileFor(rtl);
-      // }, 3000);
 
     })
     this.router.events.subscribe((event) => {
@@ -42,106 +33,10 @@ export class MenuPlacementDirective implements AfterViewInit {
     });
   }
 
-  private initialPlacementAttributeByLocalStorage(rtl: boolean): void {
-    if (true) {
-      this.document.querySelectorAll('[data-kt-menu-placement="bottom-start"]').forEach(item => {
-        const existingAttributes = item.attributes;
-        for (let i = 0; i < existingAttributes.length; i++) {
-          const attr = existingAttributes[i];
-          if (attr.name === 'data-kt-menu-placement') {
-            // Change the value of the data-kt-menu-placement attribute
-            item.setAttribute('data-kt-menu-placement', 'bottom-end');
-          }
-        }
-      });
-      this.document.querySelectorAll('[data-kt-menu-placement="right-start"]').forEach(item => {
-        // const existingAttributes = item.attributes;
-        // for (let i = 0; i < existingAttributes.length; i++) {
-        //   const attr = existingAttributes[i];
-        //   if (attr.name === 'data-kt-menu-placement') {
-        //     // Change the value of the data-kt-menu-placement attribute
-        item.setAttribute('data-kt-menu-placement', 'left-start');
-        //   }
-        // }
-      });
 
-    } else {
-      this.document.querySelectorAll('[data-kt-menu-placement="bottom-end"]').forEach(item => {
-        // const existingAttributes = item.attributes;
-        // for (let i = 0; i < existingAttributes.length; i++) {
-        //   const attr = existingAttributes[i];
-        //   if (attr.name === 'data-kt-menu-placement') {
-        //     // Change the value of the data-kt-menu-placement attribute
-        item.setAttribute('data-kt-menu-placement', 'bottom-right');
-        //   }
-        // }
-      });
-      this.document.querySelectorAll('[data-kt-menu-placement="left-start"]').forEach(item => {
-        const existingAttributes = item.attributes;
-        for (let i = 0; i < existingAttributes.length; i++) {
-          const attr = existingAttributes[i];
-          if (attr.name === 'data-kt-menu-placement') {
-            // Change the value of the data-kt-menu-placement attribute
-            item.setAttribute('data-kt-menu-placement', 'start-start');
-          }
-        }
-      });
-    }
-    // setTimeout(() => {
-    //
-    //   const isRtl = LangUtil.isRtlLang(this.translateService.getSelectedLanguage());
-    //   const elementsWithPlacement = this.document.querySelectorAll('[data-kt-menu-placement]');
-
-    //   Array.from(elementsWithPlacement).forEach((element) => {
-    //     const placementAttribute = element.getAttribute('data-kt-menu-placement');
-    //     console.log('placementAttribute', placementAttribute);
-    //     let newPlacementAttribute = placementAttribute;
-    //     if (placementAttribute && newPlacementAttribute) {
-    //       console.log(placementAttribute);
-    //       if (!isRtl) {
-    //         newPlacementAttribute = this.reversePlacementAttribute(placementAttribute);
-    //       } else {
-    //         newPlacementAttribute = this.reversePlacementAttributeForRtl(placementAttribute);
-    //       }
-
-    //       // Update the placement attribute
-    //       // this.renderer.setAttribute(element, 'data-kt-menu-placement', newPlacementAttribute);
-    //       element.setAttribute('data-kt-menu-placement', newPlacementAttribute);
-    //     } else {
-    //       alert("Error");
-    //     }
-
-    //   });
-    // }, 3000);
-
-  }
-
-  private reversePlacementAttribute(placement: string): string {
-    // Define a mapping for attribute reversal
-    const placementMapping: { [key: string]: string } = {
-      'bottom-start': 'bottom-end',
-      'bottom-end': 'bottom-start',
-      'right-start': 'left-start',
-      'left-start': 'right-start',
-      // Add more mappings as needed
-    };
-
-    return placementMapping[placement];
-  }
-
-  private reversePlacementAttributeForRtl(placement: string): string {
-    // Define a mapping for attribute reversal
-    const placementMapping: { [key: string]: string } = {
-      'bottom-end': 'bottom-start',
-      'bottom-start': 'bottom-end',
-      'left-start': 'right-start',
-      'right-start': 'left-start',
-      // Add more mappings as needed
-    };
-
-    return placementMapping[placement];
-  }
-
+/**
+   * @doc This method change the attributes of body for RTL or LRT
+   */
   private replaceBodyAttributeFor(rtl: boolean): void {
     const bodyElement = this.document.body;
 
@@ -157,9 +52,8 @@ export class MenuPlacementDirective implements AfterViewInit {
       bodyElement.setAttribute('style', 'direction: ltr');
     }
   }
-
   /**
-   * @doc This method change the style.css **lazily**
+   * @doc This method change the styles.css **lazily** with styles-rtl.css
    */
   private replaceMainStyleFileFor(rtl: boolean) {
     const head = this.document.getElementsByTagName('head')[0];
@@ -178,67 +72,7 @@ export class MenuPlacementDirective implements AfterViewInit {
 
   }
 
-
-  elementsWithPlacement: NodeListOf<Element> | [] = [];
-
-  // Function to fetch and update the elements
-  updateElementsWithPlacement(rtl: boolean) {
-
-    if (this.elementsWithPlacement.length === 0) {
-      this.elementsWithPlacement = document.querySelectorAll('[data-kt-menu-placement]');
-    }
-    if (this.elementsWithPlacement)
-      this.elementsWithPlacement.forEach((element) => {
-        const currentPlacement = element.getAttribute('data-kt-menu-placement');
-        // if (rtl) {
-        if (currentPlacement === 'bottom-start') {
-          element.setAttribute('data-kt-menu-placement', 'bottom-end');
-        } else if (currentPlacement === 'right-start') {
-          element.setAttribute('data-kt-menu-placement', 'left-start');
-        }
-        // }
-        // else {
-        //   if (currentPlacement === 'bottom-end') {
-        //     element.setAttribute('data-kt-menu-placement', 'bottom-start');
-        //   } else if (currentPlacement === 'right-start') {
-        //     element.setAttribute('data-kt-menu-placement', 'left-end');
-        //   }
-        // }
-
-      });
-  }
-
-
-  // updateMenuPlacementBasedOnRTL(isRtl: boolean) {
-  //
-  //   // Find and modify the placement attributes
-  //   const elementsToModify = document.querySelectorAll('[data-kt-menu-placement]');
-
-  //   elementsToModify.forEach((element) => {
-  //     const currentPlacement = element.getAttribute('data-kt-menu-placement');
-
-  //     // Define the new placement attributes based on RTL condition
-  //     let newPlacement;
-  //     if (isRtl) {
-  //       // RTL mode is enabled
-  //       if (currentPlacement === 'bottom-start') {
-  //         newPlacement = 'bottom-end';
-  //       } else if (currentPlacement === 'right-start') {
-  //         newPlacement = 'left-start';
-  //       } else {
-  //         newPlacement = currentPlacement; // Keep the existing placement
-  //       }
-  //     } else {
-  //       // RTL mode is not enabled, keep the existing placement
-  //       newPlacement = currentPlacement;
-  //     }
-
-  //     // Set the new placement attribute
-  //     if (newPlacement)
-  //       element.setAttribute('data-kt-menu-placement', newPlacement);
-  //   });
-  // }
-
+  //#region Replace all popover positions
   rtlInitialized = false;
   updateMenuPlacementBasedOnRTL(isRtl: boolean) {
 
@@ -319,6 +153,5 @@ export class MenuPlacementDirective implements AfterViewInit {
     }
 
   }
-
-  // document.select
+  //#endymion
 }
